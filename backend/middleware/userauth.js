bcdc777ba65fd76dc170f10a6bb0ec156b60b559
sameer das <sameerdas6143@gmail.com>
2025-03-10 11:14:09 +0530

    first-commit

--- backend/middleware/userauth.js
+++ backend/middleware/userauth.js
@@ -3,7 +3,7 @@ function isuserLoggedin(req,res,next){
     const token=  req.headers.authorization.split(" ")[1]
     
      if(token){
-        jwt.verify(token,"sam@12",(err,decode)=>{
+        jwt.verify(token,process.env.JWT_SECRET,(err,decode)=>{
           if(err) return res.status(404).json({msg:"Invalid token"})
             req.user=decode
              next()
